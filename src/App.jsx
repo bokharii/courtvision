2e9f240dd2699b04e153bcacd087b78d335f2e17
sbokhari <saaquib.bokhari@macys.com>
2026-05-25 16:59:43 -0400

    added history page + react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { useState, useEffect } from "react";
-import { Link, Routes, Route } from "react-router-dom";
-import GameCard from "./GameCard";
 import TonightPage from "./TonightPage";
+import HistoryPage from "./HistoryPage";
 import "./App.css";
+import { Link, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/history">History</Link>
-      <TonightPage />
-    </div>
+    <>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/history">History</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<TonightPage />} />
+        <Route path="/history" element={<HistoryPage />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/HistoryPage.jsx
@@ -0,0 +1,74 @@
+import GameCard from "./GameCard";
+import { useState } from "react";
+
+export default function HistoryPage() {
+  const [games, setGames] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [selectedDate, setSelectedDate] = useState("");
+  const [hasSearched, setHasSearched] = useState(false);
+
+  const yesterday = new Date();
+  yesterday.setDate(yesterday.getDate() - 1);
+  const formattedDate = yesterday.toISOString().split("T")[0];
+
+  const fetchGamesByDate = async (date) => {
+    const url = `https://api.balldontlie.io/v1/games?dates[]=${date}`;
+    try {
+      const response = await fetch(url, {
+        headers: {
+          Authorization: import.meta.env.VITE_BALLDONTLIE_KEY,
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setGames(data.data);
+      } else setError(true);
+    } catch (err) {
+      console.error(
+        "Error - something went wrong with fetch to balldontlie API",
+        err,
+      );
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <input
+          type="date"
+          max={formattedDate}
+          value={selectedDate}
+          onChange={(e) => {
+            setSelectedDate(e.target.value);
+          }}
+        ></input>
+        <button
+          onClick={() => {
+            if (!selectedDate) return;
+            setLoading(true);
+            setError(false);
+            setHasSearched(true);
+            fetchGamesByDate(selectedDate);
+          }}
+        >
+          Search
+        </button>
+      </div>
+
+      <div>Past Games</div>
+      {loading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>Something went wrong, probably too many API requests.</div>
+      ) : games.length === 0 && hasSearched ? (
+        <div>No games on this date.</div>
+      ) : (
+        games.map((game) => <GameCard game={game} key={game.id} />)
+      )}
+    </div>
+  );
+}
