4b86cb6eea3746007fa4eae1302b01cf5f6202b3
sbokhari <saaquib.bokhari@macys.com>
2026-05-25 18:49:31 -0400

    add gamecard styling

--- src/GameCard.jsx
+++ src/GameCard.jsx
@@ -4,28 +4,36 @@ import getTeamLogoUrl from "./getTeamLogoUrl";
 export default function GameCard({ game }) {
   const visitorLogo = getTeamLogoUrl(game.visitor_team.abbreviation);
   const homeLogo = getTeamLogoUrl(game.home_team.abbreviation);
+
   return (
     <article className="game-card">
-      <h1 className="game-card-matchup">
-        {visitorLogo ? (
-          <img
-            src={visitorLogo}
-            alt={`Logo for the ${game.visitor_team.full_name}`}
-          />
-        ) : null}
-        {game.visitor_team.full_name} @
-        {homeLogo ? (
-          <img
-            src={homeLogo}
-            alt={`Logo for the ${game.home_team.full_name}`}
-          />
-        ) : null}
-        {game.home_team.full_name}
+      <h1 className="game-card-teams">
+        <div className="team-row">
+          {visitorLogo ? (
+            <img
+            className="team-logo"
+              src={visitorLogo}
+              alt={`Logo for the ${game.visitor_team.full_name}`}
+            />
+          ) : null}
+          <span className="team-name"> {game.visitor_team.full_name} </span>
+        </div>
+        <p className="team-matchup-at">@</p>
+        <div className="team-row">
+          {homeLogo ? (
+            <img
+            className="team-logo"
+              src={homeLogo}
+              alt={`Logo for the ${game.home_team.full_name}`}
+            />
+          ) : null}
+          <span className="team-name"> {game.home_team.full_name} </span>
+        </div>
       </h1>
-      <h2>
+      <h2 className="game-card-score">
         {game.visitor_team_score} - {game.home_team_score}
       </h2>
-      <h3>GAME: {game.status}</h3>
+      <h3 className="game-card-status">GAME: {game.status}</h3>
     </article>
   );
 }
